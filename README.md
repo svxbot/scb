8baaf9c5b754cdeb4ee66689577df5c947f23493
svxbot <svxbot@gmail.com>
2013-11-06 18:15:46 +0400

    Make bookmarklet and add click to close

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Use this bookmarklet [Get counters](javascript:(function(){script=document.createElement('script');script.src='https://raw.github.com/svxbot/scb/master/scb.js';document.getElementsByTagName('head')[0].appendChild(script);})())
+Use this bookmarklet 
+
+```javascript
+javascript:(function(){script=document.createElement('script');script.src='https://raw.github.com/svxbot/scb/master/scb.js';document.getElementsByTagName('head')[0].appendChild(script);})()
+```
+
--- scb.js
+++ scb.js
@@ -1,10 +1,15 @@
 (function(){
-	v = '0.1.0.0';
-	img = document.createElement('img');
-	img.src = 'http://counter.yadro.ru/logo;'+document.location.host+'?28.1';
+	v='0.1.0.0';
+	id='scbImgCounter';
+	img=document.createElement('img');
+	img.src='http://counter.yadro.ru/logo;'+document.location.host+'?28.1';
 	img.style.position='fixed';
 	img.style.zIndex=9999;
 	img.style.top='100px';
 	img.style.left='20px';
-	document.getElementsByTagName('body')[0].appendChild(img);
+	img.title='click to hide';
+	img.id=id;
+	b = document.getElementsByTagName('body')[0];
+	b.appendChild(img);
+	img.onclick=function(){b.removeChild(img)};
 })()
